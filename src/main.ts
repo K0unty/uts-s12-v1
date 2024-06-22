fa9afa16b32da42b78a5dc64949d245a78c90704
K0unty <kountiapi@pm.me>
2024-06-22 07:57:05 +0530

    add console.log Formats

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./style.css";
-import { setupCounter } from "./counter.ts";
+import _ from "lodash";
 
+// This section displays pussies
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
    <h1> Testing Imports  </h1>
@@ -8,4 +9,13 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   </div>
 `;
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+////////////////////////
+
+// This Seciton is the actual work
+
+console.log(
+  `%cTesting Lodash `,
+  "background: linear-gradient(to right, rgba(38, 2, 33, 0.906) 47%, rgba(22, 0, 103, 1) 89%); color:#FFD933; font-size: 20px; padding: 10px;font-style:italic"
+);
+
+console.log(_.shuffle([1, 2, 3]));
